3861a37fcde4103bdba5684561f4d9b8307d84ba
Liam Humphreys <liamjameshumphreys@gmail.com>
2023-08-02 11:13:13 +0100

    got toggle working

--- src/App.js
+++ src/App.js
@@ -4,15 +4,35 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 const App = () => {
- const [tasks, setTasks] = useState([
+  // Tasks to Display
+  const [tasks, setTasks] = useState([
     { id: 1, text: "Doctors Appointment", day: "Feb 5th", reminder: true },
     { id: 2, text: "Dentists Appointment", day: "Feb 7th", reminder: false },
     { id: 3, text: "School Meeting", day: "Feb 8th", reminder: true },
   ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
-      <Header title={"hello"} />
-      <Tasks tasks = {tasks}/>
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to show"
+      )}
     </div>
   );
 };
