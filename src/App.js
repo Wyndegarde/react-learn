17bb22cd55a63fcccfa2062a02cb4a2d66ed189d
Liam Humphreys <liamjameshumphreys@gmail.com>
2023-08-02 13:01:05 +0100

    add task complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   // Tasks to Display
@@ -10,7 +11,12 @@ const App = () => {
     { id: 2, text: "Dentists Appointment", day: "Feb 7th", reminder: false },
     { id: 3, text: "School Meeting", day: "Feb 8th", reminder: true },
   ]);
-
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks,newTask])
+  };
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -28,6 +34,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
